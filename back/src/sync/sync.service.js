aed11be8a8ae59820a87f64d449907aab8d0745e
misha-mi <misha.saighanov@mail.ru>
2024-10-09 14:04:38 +0600

    status removed

--- back/src/sync/sync.service.js
+++ back/src/sync/sync.service.js
@@ -104,7 +104,10 @@ export class SyncService {
     try {
       const user = await this.userRepository.getUser(id);
       if (user) {
-        return await this.userRepository.remove(id);
+        return await this.prismaClient.user.update({
+          where: { id },
+          data: { status: "removed" },
+        });
       }
       return null;
     } catch {
